4ed5b3102b83a13076b28f754d1740c785ade6a0
Luis Torres <luistjr@gmail.com>
2020-09-24 02:11:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtraction_sum = num2 - num1
+  puts subtraction_sum
+  subtraction_sum
 end
 
 def division(num1, num2)
-
+  division_answer = num2 / num1
+  puts division_answer
+  division_answer
 end
 
 def multiplication(num1, num2)
-
+  multiplication_answer = num1 * num2
+  puts multiplication_answer
+  multiplication_answer
 end
 
 def modulo(num1, num2)
-
+  modulo_answer = num2 % num1 
+  puts modulo_answer
+  modulo_answer
 end
 
 def square_root(num)
-
+  square_root_answer = num ** 
+  puts square_root_answer
+  square_root_answer
 end
